ca96dcfcc9875a02dd40ea705ee7adc564312401
vishal khichadiya <vishal.khichadiya@bistasolutions.com>
2021-06-25 13:43:44 -0400

    [CLXD-1318] updated code for bulk email invoices, Testing on server

--- clx_invoice_policy/models/account_send_invoice.py
+++ clx_invoice_policy/models/account_send_invoice.py
@@ -7,11 +7,11 @@ from odoo import fields, models, api, _
 class AccountSendInvoice(models.TransientModel):
     _inherit = 'account.invoice.send'
 
-    # def send_and_print_action(self):
-    #     for rec in self.invoice_ids.filtered(lambda x: x.state not in ['cancel','email_sent'] ):
-    #         rec.state = 'email_sent'
-    #     res = super(AccountSendInvoice, self).send_and_print_action()
-    #     return res
+    def send_and_print_action(self):
+        for rec in self.invoice_ids.filtered(lambda x: x.state not in ['cancel','email_sent'] ):
+            rec.state = 'email_sent'
+        res = super(AccountSendInvoice, self).send_and_print_action()
+        return res
 
     @api.model
     def default_get(self, fields):
@@ -27,30 +27,47 @@ class AccountSendInvoice(models.TransientModel):
             contacts = self.env["account.move"].browse(self._context.get('active_ids',self.res_id)).mapped(
                 'partner_id')
             return contacts.mapped('account_user_id').mapped('partner_id') + contacts.contacts_to_notify(group_name=self._context.get('type_name'))
-        return self.env["account.move"].browse(self._context.get('active_ids',self.res_id)).mapped(
+        self.allowed_partner_ids = self.env["account.move"].browse(self._context.get('active_ids',self.res_id)).mapped(
                 'partner_id').contacts_to_notify()
+        return self.allowed_partner_ids
 
     allowed_partner_ids = fields.One2many("res.partner", compute="get_allowed_ids", string="List of contacts")
 
-    def clx_action_send_mail(self):
-        prepeared_values = {
-            "body_html": self.body,
-            "attachment_ids": self.attachment_ids,
-            "recipient_ids": False,
-            "subject": self.subject,
-            "email_from": self.email_from,
-            "reply_to": self.reply_to,
-        }
-        array_of_recipients = []
+class MailThread(models.AbstractModel):
+    _inherit = 'mail.thread'
 
-        if self.partner_ids:
-            array_of_recipients += self.partner_ids.mapped("email")
+    @api.model
+    def _message_get_default_recipients_on_records(self, records):
+        res = super(MailThread, self)._message_get_default_recipients_on_records()
+        if records and records[0]._context.get('active_model') == 'account.move':
+            res = {}
+            for record in records:
+                recipient_ids, email_to, email_cc = [], False, False
+                if 'partner_id' in record and record.partner_id:
+                    partner_ids =record.partner_id.account_user_id.partner_id +\
+                                 record.partner_id.contacts_to_notify(group_name='Billing Contact')
+                    recipient_ids.append(partner_ids.ids)
+                elif 'email_normalized' in record and record.email_normalized:
+                    email_to = record.email_normalized
+                elif 'email_from' in record and record.email_from:
+                    email_to = record.email_from
+                elif 'partner_email' in record and record.partner_email:
+                    email_to = record.partner_email
+                elif 'email' in record and record.email:
+                    email_to = record.email
+                res[record.id] = {'partner_ids': recipient_ids, 'email_to': email_to, 'email_cc': email_cc}
+        return res
 
-        for recipient in array_of_recipients:
-            prepeared_values["email_to"] = recipient.strip()
-            Mail = self.env["mail.mail"].create(prepeared_values)
-            object_source = self.env[self.model].browse(self.res_id)
-            object_source.email_send_postprocess()
-            Mail.send()
+class MailComposer(models.TransientModel):
+    _inherit = 'mail.compose.message'
 
-        return {"type": "ir.actions.act_window_close", "infos": "mail_sent"}
+    def get_mail_values(self, res_ids):
+        res = super(MailComposer, self).get_mail_values(res_ids)
+        move_ids = self.env['account.move'].browse(res_ids)
+        for move in move_ids:
+            res[move.id]['recipient_ids'] = []
+            partner_ids = move.partner_id.account_user_id.partner_id +\
+                            move.partner_id.contacts_to_notify(group_name='Billing Contact')
+            for partner in partner_ids:
+                res[move.id]['recipient_ids'].append((4,partner.id))
+        return res
